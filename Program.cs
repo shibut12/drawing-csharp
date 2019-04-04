211cbddb9d82e6a7f26a1d756156fb839469091c
Shibu Thannikkunnath <sthannikkunnath@salesforce.com>
2019-04-04 12:58:54 -0400

    New program that generate an image file

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace drawing_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Bitmap bmp = new Bitmap(600, 400))
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.FillRectangle(Brushes.Green, 0, 0, 600, 400);
+                }
+                bmp.Save("test-image.png", ImageFormat.Png);
+            }
+            Console.WriteLine($"A new image file is created at {System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase)}\\test-image.png");
+            Console.ReadKey();
+        }
+    }
+}
